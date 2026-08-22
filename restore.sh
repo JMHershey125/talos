1f0b419f3e757033d7dac284e1b1666d9a3aec94
Joshua Hershey <joshuahershey@fastmail.com>
2026-08-22 13:05:42 -0400

    changing zsh logic

--- restore.sh
+++ restore.sh
@@ -1,2 +1,8 @@
 cp -r /Users/jmhershey125/Documents/kube/* /Users/jmhershey125/git/talos/
-source /Users/jmhershey125/git/talos/.zshrc
+cat << 'EOF' >> ~/.zshrc
+
+# Talos Configuration Defaults
+export TALOSCONFIG="$HOME/talos/talosconfig"
+export TALOS_ENDPOINTS="10.74.5.87 10.74.5.69 10.74.5.216"
+export TALOS_NODES="10.74.5.87"
+EOF
